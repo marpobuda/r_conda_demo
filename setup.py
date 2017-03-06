c870638fa88a8f9fa976b2277649df649a206ad3
Marjean Pobuda <marj8502@esri.com>
2017-03-05 17:35:16 -0800

    add files from network repo to personal repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='r-conda-demo',
+      version='1.1',
+      description='Test 1',
+      author='Marjean Pobuda',
+      author_email='mpobuda@esri.com',
+      url='http://www.arcgis.com',
+      license='ESRI', )
